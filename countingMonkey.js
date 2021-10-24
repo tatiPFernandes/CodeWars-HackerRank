e7203b2525b30cdc40929e49efb2d234bc989800
Tati Fernandes <tatifernandes026@gmail.com>
2021-10-24 20:34:07 +0100

    Counting Monkey

---
+++ countingMonkey.js
@@ -0,0 +1,14 @@
+
+function monkeyCount(n) {
+    
+      
+     let monkey = [];
+      
+      for(let i = 1; i <= n; i++){
+       monkey.push(i);
+      
+    }
+      return monkey;
+      }
+      console.log(monkeyCount(20));
+    
