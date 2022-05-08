252213b32f8334b2b915deb8d6a4cb48092590ec
V H <81470166+tatiPFernandes@users.noreply.github.com>
2022-05-08 20:14:31 +0100

    paymentMethod-refactored

--- paymentMethod.js
+++ paymentMethod.js
@@ -1,14 +1,16 @@
 const paymentMethod = (product) => {
     let discout = 10;
+    let installment = 4;
 
     let total = parseFloat(product).toFixed(2)
     console.log(`Product price: £${total}`)
     
+    installment = parseFloat((total / installment)).toFixed(2)
+    console.log(`Monthly payment: £${installment}`)
+
     let onceOffPayment = total - ((discout / 100) * total)
     console.log(`Once-off-payment: £${total- parseFloat((discout / 100) * total).toFixed(2)}`)
 
-    let installment = parseFloat((total / 3)).toFixed(2)
-    console.log(`Monthly payment: £${installment}`)
 
     return total;
 
