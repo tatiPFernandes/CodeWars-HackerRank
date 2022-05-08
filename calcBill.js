b915d3576aa1acbd94d205706aed43fb9ca01779
V H <81470166+tatiPFernandes@users.noreply.github.com>
2022-05-08 19:59:24 +0100

    paymentMethod

--- calcBill.js
+++ calcBill.js
@@ -1,10 +1,10 @@
 const bill = (order) =>{
 let serviceTax = 10
 let total = 0
-total = order + parseFloat((serviceTax/100).toFixed(2) * order)
+total = parseFloat(order + (serviceTax/100) * order).toFixed(2)
 console.log(`Total order : ${order}`)
 console.log(`Total service tax: ${((serviceTax/100) * order)}`) 
 return total
 }
 
-console.log(`Total bill is : £${bill(100.50)}`)
+console.log(`Total bill is : £${parseFloat(bill(100.50)).toFixed(2)}`)
---
+++ storePayment.js
@@ -0,0 +1,15 @@
+const storePayment = (product) => {
+    let discout = 10;
+    let total = parseFloat(product).toFixed(2)
+    
+    let onceOffPayment = total - ((discout / 100) * total)
+    console.log(`Once-off-payment: £${total- parseFloat((discout / 100) * total).toFixed(2)}`)
+
+    let installment = parseFloat((total / 3)).toFixed(2)
+    console.log(`Monthly payment: £${installment}`)
+
+    return total;
+
+}
+
+console.log(`Product price: £${storePayment(60.83)}`)
